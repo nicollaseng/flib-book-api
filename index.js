85036e9af49135eb8f8509eeead37fcfcff848e2
Nicollas Carvalho <n.matheuscarvalho@gmail.com>
2019-06-30 23:50:04 -0300

    removed husky

--- index.js
+++ index.js
@@ -2,6 +2,8 @@ import express from 'express'
 import bodyParser from 'body-parser'
 import cors from 'cors'
 import routes from './src/index'
+var emoji = require('node-emoji')
+
 require('dotenv').config()
 
 const app = express()
@@ -15,4 +17,4 @@ app.use(express.static('public'))
 
 routes(app)
 
-app.listen(PORT, () => console.log(`Express has been started ${PORT}`))
+app.listen(PORT, () => console.log(`Server started ${emoji.get('rocket')} at ${PORT}`))
--- src/routes/books/list.js
+++ src/routes/books/list.js
@@ -1,11 +1,11 @@
-// import Customer from '../../models/Customer'
+require('dotenv')
+const https = require('https');
 
-// export default async (req, res) => {
-//   let customers = [];
-//   try {
-//     customers = await Customer.find({})
-//     return res.json({ customers })
-//   } catch (err){
-//     return res.status(204).json({ customers })
-//   }
-// }
+export const searchBook =  (req, res) => {
+  let book = req.body.search
+  try {
+    https.get(`https://www.googleapis.com/books/v1/volumes?q=${book}&orderBy=newest&&printType=books&filter=partial&key=${process.env.GOOGLE_BOOKS_KEY}`)
+  } catch (err){
+    return res.status(404).json({ error: 'Sorry, something went wrong :(' })
+  }
+}
