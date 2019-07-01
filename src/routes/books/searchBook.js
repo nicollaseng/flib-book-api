f638fd53d6f61506463dcfbb67bf3ce276c0cfba
Nicollas Carvalho <n.matheuscarvalho@gmail.com>
2019-07-01 00:55:36 -0300

    validation for missing list parameters

--- src/routes/books/searchBook.js
+++ src/routes/books/searchBook.js
@@ -4,19 +4,21 @@ const axios = require('axios');
 export default async (req, res) => {
   let book = req.query.q
 
-  console.log('LIVRO AQUI', book)
+  if(book){
+    const params = {
+      q: book,
+      key: process.env.GOOGLE_BOOKS_KEY
+    };
 
-  const params = {
-    q: book,
-    key: process.env.GOOGLE_BOOKS_KEY
-  };
-
-  try {
-   axios.get(`https://www.googleapis.com/books/v1/volumes`, { params })
-    .then(response => res.send(response.data.items))
-    .catch(err => res.send(err))
-  } catch (err){
-    console.log(err)
-    return res.status(404).json({ error: 'Sorry, something went wrong :(' })
+    try {
+     axios.get(`https://www.googleapis.com/books/v1/volumes`, { params })
+      .then(response => res.send(response.data.items))
+      .catch(err => res.send(err))
+    } catch (err){
+      console.log(err)
+      return res.status(404).json({ error: 'Sorry, something went wrong :(' })
+    }
+  } else {
+    return res.status(404).json({ error: 'Please insert valid parameter :(' })
   }
 }
